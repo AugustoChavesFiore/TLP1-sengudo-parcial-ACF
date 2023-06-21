8331eedda5be1fcbdc495566fed6c132d1681436
Augusto Chaves Fiore <augustochavesfiore@gmail.com>
2023-06-21 14:35:59 -0300

    modelo reserva creado

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,1 +1,43 @@
 // TODO: Crear modelo de datos de Reserva
+const { sequelize, DataTypes } = require('../db');
+
+const Reserva = sequelize.define('reserva', {
+    // Model attributes are defined here
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    fecha: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    idReserva: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
+}, {
+    // Other model options go here
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+    tableName: 'reserva'
+});
+
+// Crear tabla si no existe
+Reserva.sync();
+
+module.exports = Reserva;
